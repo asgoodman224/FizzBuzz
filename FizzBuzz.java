96f5120f7cc10b54b99424beac7d6d2d6b3af8ea
asgoodman224 <asgoodman@quinnipiac.edu>
2024-03-26 19:49:26 -0400

    MainCommit

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,6 +1,17 @@
 package FizzBuzz;
-class FizzBuzz {
+public class FizzBuzz {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
+
